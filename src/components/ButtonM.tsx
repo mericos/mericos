3c42d99bf8258ba68424726004be3c0a504da3f3
claudi_tm <ccaducarlos@gmail>
2023-04-24 23:55:13 +0200

    created variations of buttons

--- src/components/ButtonM.tsx
+++ src/components/ButtonM.tsx
@@ -8,18 +8,9 @@ interface ButtonProps {
 
 export function ButtonM(props: ButtonProps) {
 	return (
-		<Button
-			bg="primary"
-			padding={"0.3125rem 1.5rem"}
-			_hover={{ boxShadow: "0px 4px 4px 1px rgba(255, 106, 0, 0.25)" }}
-			_active={{
-				boxShadow: "inset 2px 2px 4px 4px rgba(0, 0, 0, 0.25)",
-			}}
-			_disabled={{
-				bg: "rgba(255, 106, 0, 0.5)",
-			}}
-		>
-			<Text color={"white"}>{props.text}</Text>
-		</Button>
+		<>
+			<Button variant={"secondary"}>{props.text}</Button>
+			<Button variant={"primary"}>{props.text}</Button>
+		</>
 	);
 }
--- src/style/customTheme.ts
+++ src/style/customTheme.ts
@@ -14,8 +14,45 @@ export const customTheme = extendTheme({
 		taglines_size: "1.953rem",
 		body_size: "1rem",
 		card_heading_size: "1.25rem",
-
-	}
+	},
+	components: {
+		Button: {
+			// default styles for solid buttons
+			baseStyle: {
+				borderRadius: "0.25rem",
+				padding: "0.3125rem 1.5rem",
+			},
+			variants: {
+				// styles for solid buttons with brand color
+				primary: {
+					bg: "primary",
+					color: "white",
+					_hover: {
+						bg: "brand.400",
+						boxShadow: "0px 4px 4px 1px rgba(255, 106, 0, 0.25)",
+					},
+					_active: {
+						boxShadow: "inset 2px 2px 4px 4px rgba(0, 0, 0, 0.25)",
+					},
+					_disabled: {
+						bg: "rgba(255, 106, 0, 0.5)",
+					},
+				},
+				// styles for outline buttons with brand color
+				secondary: {
+					bg: "#FFFFFF",
+					color: "#E85D04",
+					border: "1px solid",
+					_hover: {
+						boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+					},
+					_active: {
+						boxShadow: "inset 2px 2px 4px 4px rgba(0, 0, 0, 0.25)",
+					},
+				},
+			},
+		},
+	},
 });
 
 // export default customTheme;
