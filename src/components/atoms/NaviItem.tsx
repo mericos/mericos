1f89b02e8f0ccf36c6fe3717fd4cafc3c7d0cc90
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-05-07 12:40:18 +0200

    feat: create NavItem

--- src/components/atoms/NaviItem.tsx
+++ src/components/atoms/NaviItem.tsx
@@ -1,1 +1,13 @@
-    
+import { Link } from '@chakra-ui/react'
+
+interface NavItemProps {
+    link : string,
+    text: string,
+    color : string,
+}
+
+export function NavItem (props: NavItemProps) {
+    return(
+        <Link href={props.link} color={props.color}>{props.text}</Link>
+    )
+}
