03ad13fc8ae457664bed522b1d40fa5ade02f60c
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-05-08 23:24:16 +0200

    refactor: test componets

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,4 +1,4 @@
-import { Box, useBreakpointValue } from "@chakra-ui/react";
+import { Box, HStack, useBreakpointValue } from "@chakra-ui/react";
 import { Navbar } from "./Navbar";
 import { MealCard } from "./molecules/MealCard";
 
@@ -16,12 +16,18 @@ export function Introduction() {
 		},
 	) ?? "phone"
 	return (
-		<Box width="full" align="center" >
-			<Navbar type={"primary"} size={size} navigationState={"not_authenticated"} />
-			<MealCard url="https://www.simplyrecipes.com/thmb/2MQuChhZANaSSxdL1a0tA6nBgmQ=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__09__easy-pepperoni-pizza-lead-4-82c60893fcad4ade906a8a9f59b8da9d.jpg"
-			heading={"Heading"}
-			price={0}
-			description={"description"}/>
+		<Box width="full" align="center">
+			<Navbar type={"primary"} size={size} navigationState={"authenticated"} />
+			<HStack maxWidth={"7xl"}>
+				<MealCard 
+				type="secondary"
+				url="https://www.simplyrecipes.com/thmb/2MQuChhZANaSSxdL1a0tA6nBgmQ=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__09__easy-pepperoni-pizza-lead-4-82c60893fcad4ade906a8a9f59b8da9d.jpg"
+				heading={"Heading"}
+				price={0}/>
+				<MealCard url="https://www.estadao.com.br/resizer/CST2BU9c4a41u6CjapTOgobBc0o=/720x503/filters:format(jpg):quality(80):focal(1180x595:1190x605)/cloudfront-us-east-1.images.arcpublishing.com/estadao/GUOGMQ4FRJIUPAWMYLE4WNA3SY.jpg"
+				heading={"Heading"}
+				price={0}/>
+			</HStack>
 		</Box>
 	);
 }
