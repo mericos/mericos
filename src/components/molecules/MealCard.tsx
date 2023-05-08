c939cd3dc2df69f7d75c7f59c1b4c6249bbbfe73
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-05-08 17:05:48 +0200

    refactor: add width and justify button

--- src/components/molecules/MealCard.tsx
+++ src/components/molecules/MealCard.tsx
@@ -11,20 +11,21 @@ interface MealCardProps {
 export function MealCard(props: MealCardProps) {
     const {heading, price, url} = props;
     return (
-        <Card maxW='sm' textAlign={"start"}>
-            <CardBody>
+        <Card maxW={"max-content"} textAlign={"start"} padding={10}>
+            <CardBody color={"customColors.primary.500"}>
                 <Image
                 src={url}
                 borderRadius='md'
+                width={64}
                 />
                 <Stack mt='6' spacing='3'>
                 <Heading>{heading}</Heading>
-                <Text >
+                <Text color={"customColors.text_color_dark.normal"}>
                     ${price}
                 </Text>
                 </Stack>
             </CardBody>
-            <CardFooter>
+            <CardFooter justifyContent={"center"}>
                 <ButtonM type="primary" text='Adicione'/>
             </CardFooter>
             </Card>
