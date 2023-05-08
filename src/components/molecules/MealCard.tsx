07372634faed746a5c9ad669b972e7b4fa2d991a
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-05-08 22:18:26 +0200

    refactor: change card appearance

--- src/components/molecules/MealCard.tsx
+++ src/components/molecules/MealCard.tsx
@@ -1,4 +1,4 @@
-import { Button, ButtonGroup, Card, CardBody, CardFooter, Divider, Heading, Image, Stack, Text } from "@chakra-ui/react";
+import { Card, CardBody, CardFooter, CardHeader, Heading, Image, Stack, Text } from "@chakra-ui/react";
 import { ButtonM } from "../atoms/ButtonM";
 
 interface MealCardProps {
@@ -11,21 +11,26 @@ interface MealCardProps {
 export function MealCard(props: MealCardProps) {
     const {heading, price, url} = props;
     return (
-        <Card maxW={"max-content"} textAlign={"start"} padding={10}>
-            <CardBody color={"customColors.primary.500"}>
+        <Card maxWidth={"max-content"} textAlign={"start"}  variant={"outline"} p={0} m={10}>
+            <CardHeader p={0}>
                 <Image
+                fit="cover"
                 src={url}
                 borderRadius='md'
-                width={64}
+                maxBlockSize={'xs'}
+                w={"xs"}
+                h={"xs"}
                 />
-                <Stack mt='6' spacing='3'>
-                <Heading>{heading}</Heading>
+            </CardHeader>
+            <CardBody color={"customColors.primary.500"} py={0} >
+                <Stack mt='0' spacing='0'>
+                <Heading size={"md"}>{heading}</Heading>
                 <Text color={"customColors.text_color_dark.normal"}>
-                    ${price}
+                    {price} Mzn
                 </Text>
                 </Stack>
             </CardBody>
-            <CardFooter justifyContent={"center"}>
+            <CardFooter justifyContent={"center"} >
                 <ButtonM type="primary" text='Adicione'/>
             </CardFooter>
             </Card>
