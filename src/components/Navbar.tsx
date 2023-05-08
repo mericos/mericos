11e6e1cae99b2126f2bfaf709078c0ae1315cf0c
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-05-08 22:58:23 +0200

    refactor: change naming, destructure elements...

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 import { Box, Flex, Text } from "@chakra-ui/react";
 import { ArrowBackIcon } from "@chakra-ui/icons";
 import { NavItem } from "./atoms/NaviItem";
-
 import mericosLogo from "../assets/logo/only-logo.svg";
 import mericosLogoWhite from "../assets/logo/only-logo-white.svg";
 import { ButtonM } from "./atoms/ButtonM";
@@ -13,73 +12,73 @@ interface NavbarProps {
   navigationState: "authenticated" | "not_authenticated" | "goBack";
 }
 
-function getOptions(navState: NavbarProps["navigationState"]) {
-  switch (navState) {
-    case "authenticated":
-      return [
-        { name: "Menu", link: "" },
-        { name: "Encomenda", link: "" },
-        { name: "Historico", link: "" },
-      ];
-    case "not_authenticated":
-      return [
-        { name: "Menu", link: "" },
-        { name: "Login", link: "" },
-        { name: "Registro", link: "" },
-      ];
-    default:
-      return [];
-  }
+function getNavItems(navState: NavbarProps["navigationState"]) {
+  const navItems = [
+    { name: "Menu", link: "" },
+    { name: navState === "authenticated" ? "Encomenda" : "Login", link: "" },
+    { name: navState === "authenticated" ? "Historico" : "Registro", link: "" },
+  ];
+
+  return navItems.filter((navItem) => navItem.name);
 }
 
-export function Navbar(props: NavbarProps) {
-  const isPrimaryType = props.type === "primary";
-  // color properties
-  const properties = {
-    primaryColor: isPrimaryType ? "customColors.primary.500" : "white",
-    background: isPrimaryType ? "white" : "customColors.primary.500",
-    image: isPrimaryType ? mericosLogo : mericosLogoWhite,
-  };
-  //left side of the navbar
-  const leftSide = (
+function NavbarLeft({ properties }: { properties: any }) {
+  return (
     <Flex className="logo" alignItems="center" gap="0.625rem">
       <img src={properties.image} alt="mericos-logo" />
-      <Text fontSize="card_heading_size" fontWeight={"bold"} color={properties.primaryColor} >
+      <Text fontSize="card_heading_size" fontWeight="bold" color={properties.primaryColor}>
         Mericos
       </Text>
     </Flex>
   );
-    //right side of the navbar
-  const rightSide = (
-    props.size === "phone" ? (
-              /* nav items, depending on whether they are authenticated or not */
-              props.navigationState === "authenticated" ?(
-              <HamburguerMenu
-                color={properties.background}
-                background_color={properties.primaryColor}
-                options={getOptions(props["navigationState"])}
-              />) :(
-                <Flex 
-                direction={"row"}  
-                alignItems={"center"} 
-                gap={"2"}>
-                  <NavItem text="Menu" link="" color={properties.primaryColor}/>
-                  <ButtonM children="Login" type={props.type === "primary" ? "primary" : "secondary"}/>
-                </Flex>
-              )
-            ) : (
-              <Flex direction="row" alignItems="center" gap={10}>
-                {getOptions(props.navigationState).map((item) => (
-                  <NavItem
-                    key={item.name}
-                    text={item.name}
-                    link={item.link}
-                    color={properties.primaryColor}
-                  />
-                ))}
-              </Flex>
-            )
+}
+
+function NavbarRight({ properties, navProps }: { properties: any, navProps: NavbarProps }) {
+  const { navigationState, size } = navProps
+  const isPhone = size === "phone";
+
+  if (isPhone) {
+    if (navigationState === "authenticated") {
+      return (
+        <HamburguerMenu
+          color={properties.background}
+          background_color={properties.primaryColor}
+          options={getNavItems(navigationState)}
+        />
+      );
+    }
+
+    return (
+      <Flex direction="row" alignItems="center" gap={2}>
+        <NavItem text="Menu" link="" color={properties.primaryColor} />
+        <ButtonM children="Login" type={properties.buttonType} />
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex direction="row" alignItems="center" gap={10}>
+      {getNavItems(navigationState).map((item) => (
+        <NavItem
+          key={item.name}
+          text={item.name}
+          link={item.link}
+          color={properties.primaryColor}
+        />
+      ))}
+    </Flex>
   );
+}
+
+export function Navbar(props: NavbarProps) {
+  const isPrimaryType = props.type === "primary";
+
+  const properties = {
+    primaryColor: isPrimaryType ? "customColors.primary.500" : "white",
+    background: isPrimaryType ? "white" : "customColors.primary.500",
+    image: isPrimaryType ? mericosLogo : mericosLogoWhite,
+    buttonType: props.type === "primary" ? "primary" : "secondary",
+  };
 
   const backArrow = (
     <ArrowBackIcon color={properties.primaryColor} boxSize={6} />
@@ -97,9 +96,9 @@ export function Navbar(props: NavbarProps) {
         {props.navigationState !== "goBack" ? (
           <>
             {/* logo and company name */}
-            {leftSide}
+            <NavbarLeft properties={properties}/>
             {/* right side navbar part */}
-            {rightSide}
+            <NavbarRight properties={properties} navProps={props}/>
           </>
         ) : (
           // go back Navbar
