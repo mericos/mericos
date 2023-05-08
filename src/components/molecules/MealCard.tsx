fbc43e8644b6449e816aa25f073fbdf9e8fa492c
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-05-08 22:31:31 +0200

    refactor: remove and improve code

--- src/components/molecules/MealCard.tsx
+++ src/components/molecules/MealCard.tsx
@@ -1,38 +1,27 @@
-import { Card, CardBody, CardFooter, CardHeader, Heading, Image, Stack, Text } from "@chakra-ui/react";
+import { Card, CardBody, CardFooter, Heading, Image, Stack, Text } from "@chakra-ui/react";
 import { ButtonM } from "../atoms/ButtonM";
 
 interface MealCardProps {
-    url: string;
-    heading: string;
-    price: number;
-    description: string;
+  url: string;
+  heading: string;
+  price: number;
 }
 
-export function MealCard(props: MealCardProps) {
-    const {heading, price, url} = props;
-    return (
-        <Card maxWidth={"max-content"} textAlign={"start"}  variant={"outline"} p={0} m={10}>
-            <CardHeader p={0}>
-                <Image
-                fit="cover"
-                src={url}
-                borderRadius='md'
-                maxBlockSize={'xs'}
-                w={"xs"}
-                h={"xs"}
-                />
-            </CardHeader>
-            <CardBody color={"customColors.primary.500"} py={0} >
-                <Stack mt='0' spacing='0'>
-                <Heading size={"md"}>{heading}</Heading>
-                <Text color={"customColors.text_color_dark.normal"}>
-                    {price} Mzn
-                </Text>
-                </Stack>
-            </CardBody>
-            <CardFooter justifyContent={"center"} >
-                <ButtonM type="primary" text='Adicione'/>
-            </CardFooter>
-            </Card>
-    )
+export function MealCard({ url, heading, price }: MealCardProps) {
+  return (
+    <Card maxW="xs" borderRadius="md" overflow="hidden" boxShadow="lg">
+      <Image src={url} alt={heading} h={'xs'} w={'xs'} objectFit="cover" />
+      <CardBody>
+        <Stack spacing={2} align={"start"}>
+          <Heading size="md" color="customColors.primary.500">{heading}</Heading>
+          <Text fontWeight="bold" color="customColors.text_color_dark.normal">
+            {price} Mzn
+          </Text>
+        </Stack>
+      </CardBody>
+      <CardFooter>
+        <ButtonM type="primary" children="Adicione" />
+      </CardFooter>
+    </Card>
+  );
 }
