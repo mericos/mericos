308112c7b85f2fc3e099cc0f6db683df476818d5
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-05-08 23:23:23 +0200

    feat: change appearance based on card type

--- src/components/molecules/MealCard.tsx
+++ src/components/molecules/MealCard.tsx
@@ -5,22 +5,27 @@ interface MealCardProps {
   url: string;
   heading: string;
   price: number;
+  type?: "primary" | "secondary"
 }
 
-export function MealCard({ url, heading, price }: MealCardProps) {
+export function MealCard({ url, heading, price, type="primary" }: MealCardProps) {
+    const primaryColor = type === "primary" ? "white"  : "customColors.primary.500"
+    const background = type ==="primary" ? "customColors.primary.500" : "white"
+    const textColor = type === "primary" ? "white" : "customColors.text_color_dark.normal"
+    
   return (
-    <Card maxW="xs" borderRadius="md" overflow="hidden" boxShadow="lg">
+    <Card maxW="xs" borderRadius="md" overflow="hidden" boxShadow="lg" background={background}>
       <Image src={url} alt={heading} h={'xs'} w={'xs'} objectFit="cover" />
       <CardBody>
         <Stack spacing={2} align={"start"}>
-          <Heading size="md" color="customColors.primary.500">{heading}</Heading>
-          <Text fontWeight="bold" color="customColors.text_color_dark.normal">
+          <Heading size="md" color={primaryColor}>{heading}</Heading>
+          <Text fontWeight="bold" color={textColor}>
             {price} Mzn
           </Text>
         </Stack>
       </CardBody>
       <CardFooter justifyContent={"center"}>
-        <ButtonM type="primary" children="Adicione" />
+        <ButtonM type={type === "primary"? "secondary" : type} children="Adicione" />
       </CardFooter>
     </Card>
   );
