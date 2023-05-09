b5bac9de76e9b9c05c1007c332d1dc9bcf13f8e7
claudi_tm <ccaducarlos@gmail>
2023-05-09 14:36:12 +0200

    refactor; button props

--- src/components/atoms/ButtonM.tsx
+++ src/components/atoms/ButtonM.tsx
@@ -1,14 +1,16 @@
 import { Button } from "@chakra-ui/react";
+import { ReactElement } from "react";
 
 interface ButtonProps {
   type?: "primary" | "secondary";
-  children: string;
+  text: string;
+  icon?: ReactElement;
 }
 
-export function ButtonM({ type = "primary", children }: ButtonProps) {
+export function ButtonM({ type = "primary", text, icon }: ButtonProps) {
   return (
-    <Button px={8} variant={type}>
-      {children}
+    <Button px={8} variant={type} rightIcon={icon}>
+      {text}
     </Button>
   );
 }
