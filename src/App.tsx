0709e7d1eb30c21d0aa2272b2b416fa149901c67
claudi_tm <ccaducarlos@gmail>
2023-05-10 21:30:49 +0200

    style: created switch component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { Box, ChakraProvider, Flex } from "@chakra-ui/react";
+import { Box, ChakraProvider, Flex, Switch } from "@chakra-ui/react";
 import { Navbar } from "./components/molecules/Navbar";
 import { Introduction } from "./components/Introduction";
 import { customTheme } from "./style/customTheme";
+import { SwitchM } from "./components/atoms/SwitchM";
 
 export function App() {
 	return (
 		<>
 			<ChakraProvider theme={customTheme}>
 				<Flex fontFamily={"Roboto"} justifyContent="center">
-					<Introduction />
+					{/* <Introduction /> */}
+					<SwitchM/>
 				</Flex>
 			</ChakraProvider>
 		</>
