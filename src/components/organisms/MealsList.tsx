8cedf6656015702818442420816dbdd812f6e723
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-05-11 22:37:03 +0200

    feat: create a meals card list

---
+++ src/components/organisms/MealsList.tsx
@@ -0,0 +1,16 @@
+import { HStack } from "@chakra-ui/react";
+import { MealCard, MealCardProps } from "../molecules/MealCard";
+
+interface MealsListProps {
+    meals : Array<MealCardProps>
+}
+
+export function MealsList (props: MealsListProps) {
+    const {meals} = props
+    const mealCards = meals.map(({mealName, url, price}) => (<MealCard url={url} mealName={mealName} price={price}/>))
+    return(
+        <HStack overflowX={"scroll"} width={"full"}>
+            {mealCards}
+        </HStack>
+    )
+}
