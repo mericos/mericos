8b3b5598c8923ed74462af04ef60478b0e5344bd
claudi_tm <ccaducarlos@gmail>
2023-05-12 10:12:11 +0200

    feat: Loading Indicator added

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {
 import { DeviceProvider } from "./contextProviders/DeviceProvider";
 import { router } from "./router/router";
 import { customTheme } from "./style/customTheme";
+import { LoadingIndicator } from "./components/atoms/LoadingIndicator";
 
 
 
@@ -14,7 +15,8 @@ export function App() {
 			<ChakraProvider theme={customTheme}>
 				<DeviceProvider>
 					<Flex fontFamily={"Roboto"} justifyContent="center">
-						<RouterProvider router={router}/>
+						<LoadingIndicator/>
+						{/* <RouterProvider router={router}/> */}
 					</Flex>
 				</DeviceProvider>
 			</ChakraProvider>
