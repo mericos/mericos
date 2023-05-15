4492de9459b0628ae83cc60720667d09a4c56d95
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-05-15 19:41:42 +0200

    refactor: add spacing between cards

--- src/components/organisms/MealsList.tsx
+++ src/components/organisms/MealsList.tsx
@@ -9,7 +9,7 @@ export function MealsList (props: MealsListProps) {
     const {meals} = props
     const mealCards = meals.map(({mealName, url, price}) => (<MealCard key={mealName} url={url} mealName={mealName} price={price}/>))
     return(
-        <HStack overflowX={"scroll"} width={"full"} height={"-webkit-fit-content"} padding={"8"}>
+        <HStack overflowX={"scroll"} width={"full"} height={"-webkit-fit-content"} padding={"8"} spacing={4}>
             {mealCards}
         </HStack>
     )
