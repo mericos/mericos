71a83544b36878315f06febeae3f3c3404c9ecae
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-05-16 22:51:28 +0200

    claudio changes

---
+++ src/components/atoms/SwitchM.tsx
@@ -0,0 +1,10 @@
+import { Switch } from "@chakra-ui/react";
+
+export function SwitchM() {
+	return (
+		<>
+			{/* colorScheme={"primary.500"} */}
+			<Switch colorScheme="primary.500" size="md"></Switch>
+		</>
+	);
+}
