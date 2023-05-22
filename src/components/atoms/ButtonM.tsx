3f37bd64c4aa5807de16ceed9164c52440d8a978
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-05-22 23:56:15 +0200

    feat: make possible to make actions onClick

--- src/components/atoms/ButtonM.tsx
+++ src/components/atoms/ButtonM.tsx
@@ -5,12 +5,13 @@ interface ButtonProps {
   type?: "primary" | "secondary";
   text: string;
   icon?: ReactElement;
-  isSubmit? : boolean
+  isSubmit? : boolean;
+  onClick?: () => void;
 }
 
-export function ButtonM({ type = "primary", text, icon, isSubmit = false }: ButtonProps) {
+export function ButtonM({ type = "primary", text, icon, isSubmit = false, onClick}: ButtonProps) {
   return (
-    <Button w={"100%"} type={isSubmit ? "submit" : "button"} px={8} variant={type} rightIcon={icon} maxWidth={"-webkit-fit-content"}>
+    <Button w={"100%"} type={isSubmit ? "submit" : "button"} px={8} variant={type} rightIcon={icon} maxWidth={"-webkit-fit-content"} onClick={onClick}>
       {text}
     </Button>
   );
