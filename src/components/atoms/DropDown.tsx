6fc96e84e1e42428357b0e56b73554332ddbbade
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-07-04 21:38:30 +0200

    feat: add dropdown

---
+++ src/components/atoms/DropDown.tsx
@@ -0,0 +1,20 @@
+import { ChevronDownIcon } from '@chakra-ui/icons'
+import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
+import { NavItem } from './NaviItem'
+
+interface DropDownProps {
+    options: Array<{name: string, link: string}>
+}
+
+export function DropDown ({options}: DropDownProps) {
+    return(
+        <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />} bg={"inherit"} _active={{bg: "inherit"}} _hover={{bg: "inherit"}} color={"primary.500"}>
+                Actions
+            </MenuButton>
+            <MenuList>
+                {options.map(option => <MenuItem as='a' href={option.link} color={"primary.500"}>{option.name}</MenuItem> )}
+            </MenuList>
+        </Menu>
+    )
+}
