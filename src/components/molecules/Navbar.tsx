5933324476440810584ca15fb02ef5994586b1ac
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-07-04 21:39:56 +0200

    feat: add condition to navbar based on sizes pt.1

--- src/components/molecules/Navbar.tsx
+++ src/components/molecules/Navbar.tsx
@@ -6,11 +6,13 @@ import mericosLogo from "../../assets/logo/only-logo.svg";
 import { ButtonM } from "../atoms/ButtonM";
 import { NavItem } from "../atoms/NaviItem";
 import { HamburguerMenu } from "./HamburguerMenu";
+import { DropdownList } from "./DropdownList";
+import { DropDown } from "../atoms/DropDown";
 
 interface NavbarProps {
 	text?: string;
 	type?: "primary" | "secondary";
-	size: "phone" | "laptop" | "universal";
+	size: "phone" | "laptop" | "tablet" | "universal";
 	navigationState: "authenticated" | "not_authenticated" | "goBack";
 	children? : JSX.Element[];
 }
@@ -25,30 +27,43 @@ function getNavItems(navState: NavbarProps["navigationState"]) {
 	const navItems = [
 		{ name: "Menu", link: "/menu" },
 	];
-	const items = navState == "authenticated" ? [{
-			name: "Encomenda",
-			link: "/delivery",
+	const items = navState == "authenticated" ? [
+		{
+			name: "Home",
+			link: "/home",
+		},
+		{
+			name: "Sobre nós",
+			link: "/about",
+		},
+		{
+			name: "Carrinha",
+			link: "/cart",
 		},
 		{
 			name: "Historico",
 			link: "history",
-		},] : [{
-			name: "Login",
-			link: "/login",
-		},
-		{
-			name: "Registro",
-			link: "register",
-		},]
+		},] : [
+			// {
+			// 	name: "Login",
+			// 	link: "/login",
+			// },
+			// {
+			// 	name: "Registro",
+			// 	link: "register",
+			// },
+	]
 	for (const item of items) { (navItems.push(item))}
 
 
 	return navItems.filter((navItem) => navItem.name);
 }
 
-function NavbarLeft({ properties }: { properties: PropertiesProps }) {
+function NavbarLeft({ properties, navProps }: { properties: PropertiesProps; navProps: NavbarProps }) {
+	const { navigationState, size } = navProps;
+	const options = getNavItems(navigationState);
 	return (
-		<Flex className="logo" alignItems="center" gap="0.625rem">
+		<Flex className="logo" alignItems="center" gap="0.5rem" direction={"row"}>
 			<img src={properties.image} alt="mericos-logo" />
 			<Text
 				fontSize="card_heading_size"
@@ -57,6 +72,15 @@ function NavbarLeft({ properties }: { properties: PropertiesProps }) {
 			>
 				Mericos
 			</Text>
+			{size!=="phone" ? getNavItems(navigationState).filter((item,index) => size === "laptop" || index < 2).map((item, key) => (
+				<NavItem
+					key={item.name}
+					text={item.name}
+					link={item.link}
+					color={properties.primaryColor}
+				/>
+			)) : ""}
+			{size === "tablet" && options.length > 1 ? <DropDown options={options.filter((item,index) => index >= 2)}/> :""}
 		</Flex>
 	);
 }
@@ -73,34 +97,30 @@ function NavbarRight({
 	const navigate = useNavigate()
 
 	if (isPhone) {
-		if (navigationState === "authenticated") {
-			return (
+		return (
+			<Flex direction={"row"} alignItems={"center"} gap={"4"}>
+				{navigationState === "not_authenticated" ? <ButtonM text="Login" type={properties.buttonType} onClick={()=> navigate("/login")} /> : ""}
 				<HamburguerMenu
 					color={properties.background}
 					background_color={properties.primaryColor}
 					options={getNavItems(navigationState)}
 				/>
-			);
-		}
+			</Flex>
+		);
+	}
+	if (navigationState === "not_authenticated") {
 
 		return (
 			<Flex direction="row" alignItems="center" gap={2}>
-				<NavItem text="Menu" link="/menu" color={properties.primaryColor} />
 				<ButtonM text="Login" type={properties.buttonType} onClick={()=> navigate("/login")} />
+				<ButtonM text="Register" type={properties.buttonType==="primary" ? "secondary" : "primary" } onClick={()=> navigate("/register")} />
 			</Flex>
 		);
 	}
 
 	return (
-		<Flex direction="row" alignItems="center" gap={10}>
-			{getNavItems(navigationState).map((item) => (
-				<NavItem
-					key={item.name}
-					text={item.name}
-					link={item.link}
-					color={properties.primaryColor}
-				/>
-			))}
+		<Flex direction="row" alignItems="center" gap={4}>
+			buttons
 		</Flex>
 	);
 }
@@ -168,7 +188,7 @@ export function Navbar(props: NavbarProps) {
 					{navigationState !== "goBack" ? (
 						<>
 							{/* logo and company name */}
-							<NavbarLeft properties={properties} />
+							<NavbarLeft properties={properties} navProps={props} />
 							{/* right side navbar part */}
 							<NavbarRight properties={properties} navProps={props} />
 						</>
