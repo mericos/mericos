c54acfbb543b23b114626796e8bc6cb03440c540
claudi_tm <ccaducarlos@gmail>
2023-08-11 09:35:28 +0200

    feat: add nwe type of button

--- src/components/atoms/ButtonM2.tsx
+++ src/components/atoms/ButtonM2.tsx
@@ -1,8 +1,7 @@
 import { Button, ButtonProps } from "@chakra-ui/react";
 
-// Define a custom button interface that extends Chakra UI's ButtonProps
+
 interface ButtonM2Props extends ButtonProps {
-  // Add any additional custom props specific to your application
   text: string;
 }
 export function ButtonM2(props: ButtonM2Props) {
