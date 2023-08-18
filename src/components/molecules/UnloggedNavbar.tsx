bf56e79a2bc4e517dffa203af847bc4ed77fd0d6
claudi_tm <ccaducarlos@gmail>
2023-08-18 19:07:59 +0200

    feat: tooggle navigation bar

--- src/components/molecules/UnloggedNavbar.tsx
+++ src/components/molecules/UnloggedNavbar.tsx
@@ -1,11 +1,45 @@
-import { Flex, Hide, Image, Show } from "@chakra-ui/react";
-import { AiOutlineMenu } from "react-icons/ai";
-import { Icon } from "../atoms/Icon";
+import {
+	Flex,
+	Heading,
+	Hide,
+	IconButton,
+	Image,
+	List, Show,
+	UnorderedList
+} from "@chakra-ui/react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import Logo from "../../assets/logo/logo2.svg";
-import { LogoM } from "../atoms/LogoM";
 import { ButtonM2 } from "../atoms/ButtonM2";
 
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export function UnloggedNavbar() {
+	const [display, changeDisplay] = useState("none");
+	const nav_titles = [
+		<Link key={""} to={"/home"}>
+			Home
+		</Link>,
+		<Link key={""} to={""}>
+			Favorites
+		</Link>,
+		<Link key={""} to={""}>
+			Profile
+		</Link>,
+		<Link key={""} to={""}>
+			Orders
+		</Link>,
+		<Link key={""} to={""}>
+			Custom Support
+		</Link>,
+		<Link key={""} to={""}>
+			About Us
+		</Link>,
+		<Link key={""} to={""}>
+			Mericos Carrers
+		</Link>,
+	];
+
 	return (
 		<>
 			<Flex
@@ -19,8 +53,71 @@ export function UnloggedNavbar() {
 					// minW={["15%", "15%", "5%"]}
 				/>
 				<Hide above="md">
-					<Icon icon={<AiOutlineMenu size={30} />} />
+					<IconButton
+						backgroundColor={"white"}
+						icon={<AiOutlineMenu size={30} />}
+						aria-label="hamburguer_menu"
+						onClick={() => changeDisplay("flex")}
+					/>
 				</Hide>
+
+				<Flex
+					flexDirection={"column"}
+					alignItems={"center"}
+					// justifyContent={"center"}
+					w={"full"}
+					h={"full"}
+					zIndex={20}
+					pos={"absolute"}
+					bgColor={"gray.500"}
+					opacity={"100%"}
+					top={"0"}
+					left={"0"}
+					overflow={"auto"}
+					color={"white"}
+					display={display}
+				>
+					
+					<Flex
+						m={"2"}
+						w={"100%"}
+						p={"4"}
+						alignItems={"center"}
+						gap={"4"}
+						justifyContent={"flex-start"}
+					>
+						<IconButton
+							icon={<AiOutlineClose size={30} />}
+							aria-label={"close_button"}
+							onClick={() => changeDisplay("none")}
+							backgroundColor={"transparent"}
+							_hover={{backgroundColor:"primary.300"}}
+						></IconButton>
+
+						<Heading fontSize={"2xl"} color={"white"}>
+							Mericos
+						</Heading>
+					</Flex>
+
+					<UnorderedList w={"100%"}>
+						{nav_titles.map((link) => (
+							<List
+								p={"3"}
+								m={"4"}
+								_hover={{ backgroundColor: "primary.300" }}
+								rounded={"lg"}
+								key={""}
+							>
+								{link}
+							</List>
+						))}
+					</UnorderedList>
+					<Flex
+						flexDir={"column"}
+						color={"white"}
+						alignItems={"center"}
+					></Flex>
+				</Flex>
 
 				<Show above="md">
 					<Flex gap={3}>
---
+++ src/style/animations.ts
@@ -0,0 +1,25 @@
+import { Flex, FlexProps } from "@chakra-ui/react"
+import { motion } from "framer-motion"
+
+
+export const animationFlex = {
+    hidden: { opacity: 1, scale: 0 }, 
+    visible: {
+        opacity: 1, 
+        scale: 1, 
+        transition: {
+            delayChildren: 0.3, 
+            staggerChildren: 0.2
+        }
+    }
+}
+
+export const itemAnimation = {
+    hidden: { y: -60, opacity: 0 },
+    visible: {
+        y: 0, 
+        opacity: 1
+    }
+}
+
+export const MotionFlex = motion<FlexProps>(Flex)
