913f8abc582482f23395c84dea64b468713bda7c
claudi_tm <ccaducarlos@gmail>
2023-09-07 07:18:31 +0200

    refractor: favorites and getting starteed

--- src/components/molecules/CardFavorite.tsx
+++ src/components/molecules/CardFavorite.tsx
@@ -1,22 +1,16 @@
 import {
-	Button,
-	ButtonGroup,
 	Card,
 	CardBody,
 	CardFooter,
 	CardProps,
-	Divider,
-	Flex,
 	Heading,
 	IconButton,
 	Image,
 	Stack,
 	Text,
 } from "@chakra-ui/react";
-import { SlArrowRight } from "react-icons/sl";
-import { ButtonM2 } from "../atoms/ButtonM2";
-import { BsCart } from "react-icons/bs";
 import { AiOutlineHeart } from "react-icons/ai";
+import ImageT from "../../assets/ImagePC.png"
 
 interface CardMProps extends CardProps {
 	card_heading: string;
@@ -28,7 +22,7 @@ export function CardFavorite(props: CardMProps) {
 		<Card maxW="sm" flexDir={"row"} align={"center"}>
 			<CardBody display={"flex"} gap={4} alignItems={"center"}>
 				<Image
-					src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+					src={ImageT}
 					alt="Green double couch with wooden legs"
 					borderRadius="full"
 					w={"24"}
