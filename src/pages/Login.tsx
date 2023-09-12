4a59bce931232f90bc6b5238e06e3081fd28e349
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-09-12 22:50:18 +0200

    refactor: change login layout

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -13,35 +13,43 @@ import {
 	Show,
 	Text
 } from "@chakra-ui/react";
-import { Formik } from "formik";
+import { Formik, useFormik } from "formik";
 import { Form, Link } from "react-router-dom";
 import * as Yup from "yup";
 import { ImFacebook } from "react-icons/im";
 import { FcGoogle } from "react-icons/fc";
-import { InputM } from "../components/atoms/InputM";
-import { ButtonM2 } from "../components/atoms/ButtonM2";
 import Logo from "../assets/logo/logo2.svg";
 
 export function Login() {
-	// const loginSchema = Yup.object().shape({
-	// 	email: Yup.string()
-	// 		.email("E-mail inválido")
-	// 		.required("Email é obrigatória"),
-	// 	password: Yup.string()
-	// 		.required("Palavra-passe é obrigatória")
-	// 		.min(8, "A palavra-passe deve ter pelo menos 8 caracteres")
-	// 		.matches(
-	// 			/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&.])[A-Za-z\d@$!%*#?&.]+$/,
-	// 			"A palavra-passe deve conter pelo menos uma letra, um número e um caractere especial"
-	// 		),
-	// });
+	const loginSchema = Yup.object().shape({
+		email: Yup.string()
+			.email("E-mail inválido")
+			.required("Email é obrigatória"),
+		password: Yup.string()
+			.required("Palavra-passe é obrigatória")
+			.min(8, "A palavra-passe deve ter pelo menos 8 caracteres")
+			.matches(
+				/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&.])[A-Za-z\d@$!%*#?&.]+$/,
+				"A palavra-passe deve conter pelo menos uma letra, um número e um caractere especial"
+			),
+	});
+	const {values, errors, touched, handleChange, handleSubmit} = useFormik({
+		initialValues: {
+			email:"",
+			password:""
+		},
+		onSubmit: (values) => {
+			alert(JSON.stringify(values, null, 2));
+		},
+		validationSchema: loginSchema
+	})
 	return (
 		<Flex w={"full"} h={"100vh"}>
-			<Flex flexDir={"column"} w={"full"}  maxW={"2xl"} width={["100%", "100%", "50%"]} padding={"8rem 4rem"} gap={"1rem"} margin={"auto"}>
+			<Flex flexDir={"column"} w={"full"}  maxW={"2xl"} width={["100%", "100%", "50%"]} padding={"0rem 4rem"} gap={"1rem"} margin={"auto"}>
 				<Flex justifyContent={"center"}>
 					<Image
 							src={Logo}
-							w={["30%", "30%", "15%"]}
+							w={["30%", "15%", "15%"]}
 							transition={"ease 0.3s"}
 							css={{
 								clipPath: "circle(50% at 50% 50%)"
@@ -52,27 +60,33 @@ export function Login() {
 					<Heading>Login</Heading>
 					<Text color={"gray.400"}>Login to continue using the app</Text>
 				</Flex>
-				<form>
+				<form onSubmit={handleSubmit}>
 					<Flex flexDir={"column"} gap={"1rem"} alignItems={"end"}>
-						<FormControl variant={"floating"} id="firstName">
+						<FormControl variant={"floating"} id="firstName" isRequired isInvalid={!!errors.email && touched.email}>
 							<Input
 								type="email"
 								name="email"
 								placeholder=""
+								onChange={handleChange}
+								value={values.email}
 								/>
 							<FormLabel>Email</FormLabel>
+							<FormErrorMessage>{errors.email}</FormErrorMessage>
 							</FormControl>
 
-							<FormControl variant={"floating"} id="lastName">
+							<FormControl variant={"floating"} id="lastName" isInvalid={!!errors.password && touched.password} isRequired>
 							<Input
 								placeholder=""
 								type="password"
 								name="password"
+								onChange={handleChange}
+								value={values.password}
 								/>
 							<FormLabel>Password</FormLabel>
+							<FormErrorMessage>{errors.password}</FormErrorMessage>
 							</FormControl>
 						<Link to={"/new-password"}><Text fontSize={"sm"} color={"gray.500"}>Forgot Password?</Text></Link>
-						<Button colorScheme="primary" w={"full"}>Login</Button>
+						<Button type="submit" colorScheme="primary" w={"full"}>Login</Button>
 					</Flex>
 					</form>
 					<Flex flexDir={"row"} alignItems={"center"} gap={"0.625rem"}>
@@ -88,12 +102,18 @@ export function Login() {
 								<ImFacebook size={24} />
 							</Button>
 					</Flex>
+					<Flex justifyContent={"center"} wrap={"wrap"}>
+						<Text textAlign={"center"}>Don't have an account?</Text>
+						<Link to={"/register"}><Text color={"primary.500"}>Register</Text></Link>
+					</Flex>
 			</Flex>
 			<Flex  w={["0%","0%","50%"]}>
 				<Image 
 				background={"gray.100"}
 				objectFit={"cover"}
-				src="https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png" 
+				w={"full"}
+				src="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg" 
+				filter={"blur(0px)"}
 				alt=""/>
 			</Flex>
 		</Flex>
