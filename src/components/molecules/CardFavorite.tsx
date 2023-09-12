7a45c5de35be108b5ed9887164cb37ca55325fb8
claudi_tm <ccaducarlos@gmail>
2023-09-12 17:07:30 +0200

    feat: added color to heart icon on click

--- src/components/molecules/CardFavorite.tsx
+++ src/components/molecules/CardFavorite.tsx
@@ -11,6 +11,7 @@ import {
 } from "@chakra-ui/react";
 import { AiOutlineHeart } from "react-icons/ai";
 import ImageT from "../../assets/ImagePC.png"
+import { useState } from "react";
 
 
 interface CardMProps extends CardProps {
@@ -19,6 +20,13 @@ interface CardMProps extends CardProps {
 }
 
 export function CardFavorite(props: CardMProps) {
+	const [isClicked, setIsClicked] = useState(false);
+
+	function handleClick() {
+		setIsClicked(!isClicked);
+		console.log(isClicked);
+		
+	}
 	return (
 		<Card flexGrow={1} flexDir={"row"} align={"center"}>
 			<CardBody display={"flex"} gap={4} alignItems={"center"}>
@@ -31,15 +39,19 @@ export function CardFavorite(props: CardMProps) {
 					objectFit={"cover"}
 				/>
 				<Stack spacing="2">
-					<Heading color={"black"} fontSize={["sm", "md"]}>{props.card_heading}</Heading>
+					<Heading color={"black"} fontSize={["sm", "md"]}>
+						{props.card_heading}
+					</Heading>
 					<Text fontSize={["sm", "md"]}>{props.card_caption}</Text>
 				</Stack>
 			</CardBody>
-			<CardFooter >
+			<CardFooter>
 				<IconButton
+					color={isClicked ? "primary.500" : "gray.700"}
 					bgColor={"white"}
-					icon={<AiOutlineHeart size={30} color="gray.500" />}
+					icon={<AiOutlineHeart size={30} />}
 					aria-label="heart"
+					onClick={handleClick}
 				/>
 			</CardFooter>
 		</Card>
