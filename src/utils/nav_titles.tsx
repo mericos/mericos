90294d47f6533a841e757be901881f4dd88ff9c7
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-09-14 09:24:24 +0200

    refactor: comment unused nav-titles

--- src/utils/nav_titles.tsx
+++ src/utils/nav_titles.tsx
@@ -3,9 +3,9 @@ export const nav_titles = [
   { name: "Favorites", path: "/favorites" },
   { name: "Profile", path: "/profile" },
   { name: "Orders", path: "/orders" },
-  { name: "Custom Support", path: "/custom-support" },
-  { name: "About Us", path: "/about-us" },
-  { name: "Mericos Carrers", path: "/carrers" },
+  // { name: "Custom Support", path: "/custom-support" },
+  // { name: "About Us", path: "/about-us" },
+  // { name: "Mericos Carrers", path: "/carrers" },
 ];
 
 
