8a3d7492134cb93b8762160ab532d8092a464b84
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-09-14 09:22:50 +0200

    refactor: status color conditions

--- src/components/atoms/OrderStatus.tsx
+++ src/components/atoms/OrderStatus.tsx
@@ -12,6 +12,7 @@ interface orderStatusProps {
 export function OrderStatus({status, text}:orderStatusProps) {
     let bgColor = ""
     let icon = null
+    let color = "primary.600"
     if( status === "canceled") {
         bgColor = "red.100"
         icon = <MdOutlineCancel boxSize={4}/>
@@ -20,22 +21,23 @@ export function OrderStatus({status, text}:orderStatusProps) {
         icon = <MinusIcon boxSize={4}/>
     }
     else {
-        bgColor = "primary.300"
+        bgColor = "primary.200"
         if (status === "travelling") {
             icon = <MdDeliveryDining boxSize={4}/>
         }
         if (status === "in place") {
             icon = <HiLocationMarker boxSize={4}/>
             bgColor="primary.500"
+            color="white"
         }
         if (status === "delivered") {
             icon = <BsCheck boxSize={4}/>
         }
     }
     return(
-        <Card flexGrow={[0,1,1]} color={status === "canceled" ? "red.400" : "primary.600"} bg={bgColor} variant={"outline"} w={"min-content"} as={Flex} flexDir={"row"} padding={"0.375rem 1rem"} gap={"0.5rem"} alignItems={"center"} justifyContent={"center"}>
+        <Card flexGrow={[0,1,1]} color={color} bg={bgColor} variant={"outline"} w={"min-content"} as={Flex} flexDir={"row"} padding={"0.375rem 1rem"} gap={"0.5rem"} alignItems={"center"} justifyContent={"center"}>
             {icon}
-            <Text color={status ==="canceled"?"red.400":"gray.700"} fontSize={"sm"} whiteSpace={"nowrap"}>{text}</Text>
+            <Text color={color} fontSize={"sm"} whiteSpace={"nowrap"}>{text}</Text>
         </Card>
     )
 }
